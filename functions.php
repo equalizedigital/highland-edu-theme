5e2dc1e463e9a9e51817b2cb19e1fbd3bbc6dcc4
Baljinder Singh <mike@wpxhouston.com>
2023-09-12 23:04:17 +0300

    Hide organizer from events plugin

--- functions.php
+++ functions.php
@@ -153,3 +153,41 @@ function categories_after_title() {
 	<?php
 }
 add_action( 'tribe_template_before_include:events/v2/list/event/venue', 'categories_after_title' );
+
+/**
+ * The Events Calendar: Remove the Organizers post type from Events.
+ *
+ * Replace instances of ORGANIZER_POST_TYPE with VENUE_POST_TYPE if you
+ * want to do so for Venues instead.
+ *
+ * @link https://theeventscalendar.com/knowledgebase/linked-post-types/
+ * @link https://gist.github.com/a521d02facbc64ce3891c9341384cc07
+ */
+function tribe_remove_organizers_from_events( $default_types ) {
+ 
+    if (
+        ! is_array( $default_types )
+        || empty( $default_types )
+        || empty( Tribe__Events__Main::ORGANIZER_POST_TYPE )
+    ) {
+        return $default_types;
+    }
+ 
+    if ( ( $key = array_search( Tribe__Events__Main::ORGANIZER_POST_TYPE, $default_types ) ) !== false ) {
+        unset( $default_types[ $key ] );
+    }
+ 
+    return $default_types;
+}
+ 
+add_filter( 'tribe_events_register_default_linked_post_types', 'tribe_remove_organizers_from_events' );
+
+function unregister_tribe_organizer_post_type() {
+	unregister_post_type( 'tribe_organizer' );
+}
+add_action( 'init', 'unregister_tribe_organizer_post_type', 20 );
+
+add_action( 'admin_menu', 'remove_my_submenu', 9999 );
+function remove_my_submenu() {
+	remove_submenu_page('edit.php?post_type=tribe_events', 'edit.php?post_type=' . Tribe__Events__Organizer::POSTTYPE );
+}
