40a3f820bbfcc72b1a6f35f5d60c1e8d95f619fb
michelmany <hello@michelmany.com>
2024-10-04 14:56:10 +0200

    fix: refactor and fix the autoplay conflict

--- template-parts/blocks/testimonial-slider/scripts.js
+++ template-parts/blocks/testimonial-slider/scripts.js
@@ -1,80 +1,58 @@
-class TestimonialSlider {
-    constructor(selector, args) {
-        this.selector = selector;
-        this.args = args;
-        this.init();
-    }
+// Function to add event listeners to buttons
+function addButtonEventListeners() {
+    const buttons = document.querySelectorAll('.button');
+    buttons.forEach((button) => {
+        button.addEventListener('keydown', (event) => {
+            if (event.keyCode === 9) {
+                event.preventDefault();
+                const activeBullet = document.querySelector('.swiper-pagination-bullet-active');
+                activeBullet.focus();
+            }
+        });
+    });
+}
 
-    init() {
-        document.addEventListener('DOMContentLoaded', () => {
-            this.testimonialSlider = new Swiper(this.selector, this.args);
-            this.addEventListeners();
+// Function to add event listeners to pagination bullets
+function addPaginationEventListeners(testimonialSlider) {
+    const paginationBullets = document.querySelectorAll('.swiper-pagination-bullet');
+    paginationBullets.forEach((bullet, index) => {
+        bullet.addEventListener('keydown', (event) => {
+            if (event.keyCode === 32) {
+                event.preventDefault();
+                bullet.click();
+            }
         });
 
-        if (window.acf) {
-            window.acf.addAction('render_block_preview/type=testimonial_slider', this.initializeBlock.bind(this));
-        }
-    }
-
-    initializeBlock(block) {
-        this.testimonialSlider = new Swiper(this.selector, this.args);
-    }
-
-    addEventListeners() {
-        this.addButtonEventListeners();
-        this.addPaginationEventListeners();
-
-        const controlBtn = document.querySelector('.hl-button-control');
-        if (controlBtn) {
-            controlBtn.addEventListener('click', () => {
-                if (controlBtn.getAttribute('aria-pressed') === 'false') {
-                    this.testimonialSlider.autoplay.stop();
-                    controlBtn.querySelector('.screen-reader-text').value = 'Play';
-                    controlBtn.querySelector('.dashicons').classList.replace('dashicons-controls-pause', 'dashicons-controls-play');
-                    controlBtn.setAttribute('aria-pressed', 'true');
-                } else {
-                    this.testimonialSlider.autoplay.start();
-                    controlBtn.querySelector('.screen-reader-text').value = 'Pause';
-                    controlBtn.querySelector('.dashicons').classList.replace('dashicons-controls-play', 'dashicons-controls-pause');
-                    controlBtn.setAttribute('aria-pressed', 'false');
-                }
-            });
-        }
-    }
-
-    addButtonEventListeners() {
-        const buttons = document.querySelectorAll('.button');
-        buttons.forEach((button) => {
-            button.addEventListener('keydown', (event) => {
-                if (event.keyCode === 9) {
-                    event.preventDefault();
-                    const activeBullet = document.querySelector('.swiper-pagination-bullet-active');
-                    activeBullet.focus();
-                }
-            });
+        bullet.addEventListener('click', () => {
+            const h2Element = testimonialSlider.slides[index].querySelector('h2');
+            if (h2Element) {
+                h2Element.focus();
+            }
         });
-    }
+    });
+}
 
-    addPaginationEventListeners() {
-        const paginationBullets = document.querySelectorAll('.swiper-pagination-bullet');
-        paginationBullets.forEach((bullet, index) => {
-            bullet.addEventListener('keydown', (event) => {
-                if (event.keyCode === 32) {
-                    event.preventDefault();
-                    bullet.click();
-                }
-            });
-
-            bullet.addEventListener('click', () => {
-                const h2Element = this.testimonialSlider.slides[index].querySelector('h2');
-                if (h2Element) {
-                    h2Element.focus();
-                }
-            });
+// Function to handle control button click event
+function handleControlButtonClick(testimonialSlider) {
+    const controlBtn = document.querySelector('.eqd-slide-button-control');
+    if (controlBtn) {
+        controlBtn.addEventListener('click', () => {
+            if (controlBtn.getAttribute('aria-pressed') === 'false') {
+                testimonialSlider.autoplay.stop();
+                controlBtn.querySelector('.screen-reader-text').textContent = 'Play';
+                controlBtn.querySelector('.dashicons').classList.replace('dashicons-controls-pause', 'dashicons-controls-play');
+                controlBtn.setAttribute('aria-pressed', 'true');
+            } else {
+                testimonialSlider.autoplay.start();
+                controlBtn.querySelector('.screen-reader-text').textContent = 'Pause';
+                controlBtn.querySelector('.dashicons').classList.replace('dashicons-controls-play', 'dashicons-controls-pause');
+                controlBtn.setAttribute('aria-pressed', 'false');
+            }
         });
     }
 }
 
+// Swiper configuration
 const testimonialSliderArgs = {
     loop: true,
     speed: 1000,
@@ -87,52 +65,23 @@ const testimonialSliderArgs = {
     },
     on: {
         afterInit: function(testimonialSlider) {
-            const buttons = document.querySelectorAll('.button');
-            buttons.forEach((button) => {
-                button.addEventListener('keydown', (event) => {
-                    if (event.keyCode === 9) {
-                        event.preventDefault();
-                        const activeBullet = document.querySelector('.swiper-pagination-bullet-active');
-                        activeBullet.focus();
-                    }
-                });
-            });
-
-            const paginationBullets = document.querySelectorAll('.swiper-pagination-bullet');
-            paginationBullets.forEach((bullet, index) => {
-                bullet.addEventListener('keydown', (event) => {
-                    if (event.keyCode === 32) {
-                        event.preventDefault();
-                        bullet.click();
-                    }
-                });
-
-                bullet.addEventListener('click', () => {
-                    const h2Element = testimonialSlider.slides[index].querySelector('h2');
-                    if (h2Element) {
-                        h2Element.focus();
-                    }
-                });
-            });
-
-            const controlBtn = document.querySelector('.hl-button-control');
-            if (controlBtn) {
-                controlBtn.addEventListener('click', () => {
-                    if (controlBtn.getAttribute('aria-pressed') === 'false') {
-                        testimonialSlider.autoplay.stop();
-                        controlBtn.querySelector('.screen-reader-text').value = 'Play';
-                        controlBtn.querySelector('.dashicons').classList.replace('dashicons-controls-pause', 'dashicons-controls-play');
-                        controlBtn.setAttribute('aria-pressed', 'true');
-                    } else {
-                        testimonialSlider.autoplay.start();
-                        controlBtn.querySelector('.screen-reader-text').value = 'Pause';
-                        controlBtn.querySelector('.dashicons').classList.replace('dashicons-controls-play', 'dashicons-controls-pause');
-                        controlBtn.setAttribute('aria-pressed', 'false');
-                    }
-                });
-            }
+            addButtonEventListeners();
+            addPaginationEventListeners(testimonialSlider);
+            handleControlButtonClick(testimonialSlider);
         },
     },
 };
 
-new TestimonialSlider('.swiper', testimonialSliderArgs);
+// Initialize Swiper on DOMContentLoaded
+document.addEventListener('DOMContentLoaded', function() {
+    new Swiper('.testimonial-slider-swiper', testimonialSliderArgs);
+});
+
+// Initialize Swiper for block previews
+const initializeBlock = function(block) {
+    new Swiper('.testimonial-slider-swiper', testimonialSliderArgs);
+};
+
+if (window.acf) {
+    window.acf.addAction('render_block_preview/type=testimonial-slider', initializeBlock);
+}
--- template-parts/blocks/testimonial-slider/testimonial-slider.php
+++ template-parts/blocks/testimonial-slider/testimonial-slider.php
@@ -24,7 +24,7 @@ if ( ! empty( $block['className'] ) ) {
 <?php if ( have_rows( 'testimonial_slider' ) ) : ?>
     <div id="<?php echo esc_attr( $id ); ?>" class="<?php echo esc_attr( $className ); ?>" role="region" aria-label="carousel">
         <div class="super-container">
-            <div class="swiper container">
+            <div class="testimonial-slider-swiper container">
                 <div class="swiper-wrapper slider">
 					<?php
 					while ( have_rows( 'testimonial_slider' ) ) : the_row();
@@ -42,6 +42,10 @@ if ( ! empty( $block['className'] ) ) {
                                     <img src="<?php echo esc_url( $testimonial_image['sizes']['large'] ); ?>"
                                          alt="<?php echo esc_attr( $testimonial_image_alt ); ?>"/>
                                     <h3 class="name"><?php echo esc_html( $testimonial_name ); ?></h3>
+                                    <button class="eqd-slide-button-control" aria-pressed="false">
+                                        <span class="screen-reader-text">Pause</span>
+                                        <span class="dashicons dashicons-controls-pause"></span>
+                                    </button>
                                     <div class="slide-ctrl play">
                                         <div class="icon" width="120" height="120">
                                             <div class="side left" x="0" y="0" width="120" height="120"
@@ -59,6 +63,7 @@ if ( ! empty( $block['className'] ) ) {
 
 					<?php endwhile; ?>
                 </div>
+                <div class="swiper-pagination"></div>
             </div>
         </div>
     </div>
