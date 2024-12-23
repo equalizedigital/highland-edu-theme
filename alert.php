803fe8d6b87eaf1d2cb145ba53d6fca69d2e8a7b
Steve Jones <steve@stevejonesdev.com>
2024-12-23 15:00:42 -0500

    updated: alert display and styling; improved accessibility with screen reader text and changed background color

--- alert.php
+++ alert.php
@@ -12,16 +12,16 @@ if($loop->have_posts()) {
     echo '<div class="super-container alert-wrapper">';
     echo '<div class="container alert-holder">';
     while ($loop->have_posts()) : $loop->the_post();
-        $alert_link = get_post_meta(get_the_id(), '_cmb_alert_url', true);
+        $alert_link = get_post_meta(get_the_ID(), '_cmb_alert_url', true);
         echo '<div class="sixteen columns alert">';
-        echo '<img src="' . get_bloginfo('template_url') . '/images/alert-icon.png" class="alert-icon" />';
+        echo '<img src="' . get_bloginfo('template_url') . '/images/alert-icon.png" class="alert-icon" alt="" role="presentation" />';
         the_title('<div class="alert-title"><h2>', '</h2></div>');
         echo '<div class="alert-body">';
-        echo utf8_truncate(get_the_content(), $max_chars = 200, $append = "...");
+        echo wp_trim_words(get_the_content(), 40, '...');
         if($alert_link){
-            echo ' <a href="' . $alert_link . '">continue reading</a>';
+            echo ' <a href="' . $alert_link . '">continue reading<span class="screen-reader-text"> ' . get_the_title() . '</span></a>';
         } else {
-            echo ' <a href="' . get_permalink() . '">continue reading</a>';
+            echo ' <a href="' . get_permalink(get_the_ID()) . '">continue reading<span class="screen-reader-text"> ' . get_the_title() . '</span></a>';
         }
         echo '</div>';
         echo '</div>';
